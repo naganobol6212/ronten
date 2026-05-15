a6d9865040a55f4cacee2fc33727c1523d3bc70a
masagin <naganobol6212@gmail.com>
2026-05-15 18:23:54 +0900

    Add summaries app and register third-party apps

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django built-in apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party apps
+    'rest_framework',
+    'corsheaders',
+    # Local apps
+    'summaries',
 ]
 
 MIDDLEWARE = [
